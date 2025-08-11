23473f6039950e158fc474bde85d153c7c8b6c95
Eliana <Eliana@DESKTOP-O31UEN9>
2025-08-10 21:31:11 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exemplo_cabecalho_rodape.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
